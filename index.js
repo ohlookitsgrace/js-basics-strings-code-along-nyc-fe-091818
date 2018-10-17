41fd420134aef6758ff14b1bb016a2c57d77742c
Grace Han <ohlookitsgrace@gmail.com>
2018-10-17 00:47:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 let currentUser = 'Grace'
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
-let shortGreeting = "Welcome, " + currentUser.
+let shortGreeting = "Welcome, " + currentUser.charAt(0)
 

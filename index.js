ed5a7b98df85e90a86b6b119a6eee52cf9a03e5a
Grace Han <ohlookitsgrace@gmail.com>
2018-10-17 00:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code in this file!
 let currentUser = 'Grace'
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser
+
